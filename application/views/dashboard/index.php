b12102701e16a25ec4fcc0009d231f48d963bcd0
unknown <bram.antyo2796@gmail.com>
2018-07-14 00:49:50 +0700

    chartjs dashboard lanjut

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,16 +1,17 @@
 <!-- <body> -->
     
 
-<div class="container">
-    <!-- <div class="row">
-        <div class="col-md-6"> -->
-        <h2>REPORT DASHBOARD KOMPLAIN KESELURUHAN</h2><hr>
+<div class="container"><br>
+<h2><b>REPORT DASHBOARD KOMPLAIN KESELURUHAN</b></h2><hr>
+    <div class="row">
+        <div class="col-sm-6"> 
+       
         
-    <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script> -->
-    <!-- <script type="text/javascript" src="<?php echo base_url(); ?>css2/Chart.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>css2/Chart.min.js"></script>
 
     <br>
-      <canvas id="myChart" style="width: 900px; height: 300px;"></canvas>
+    <canvas id="myChart1" style="width: 400px; height: 400px; border:1px solid #000000;"></canvas>
       <script type="text/javascript">
       
       <?php
@@ -23,8 +24,8 @@
       
 
    
-      var ctx = document.getElementById("myChart");
-            var myChart = new Chart(ctx, {
+      var ctx = document.getElementById("myChart1");
+            var myChart1 = new Chart(ctx, {
                 type: 'bar',
                 data: {
                     labels: <?php echo json_encode($kom);?>,
@@ -61,23 +62,25 @@
                     }
                 }
             });
-        </script> -->
-
-        <div class='tableauPlaceholder' id='viz1531410432488' style='position: relative'><noscript><a href='#'><img alt='Dashboard  ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ko&#47;KomplainDashboard&#47;Dashboard2&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='KomplainDashboard&#47;Dashboard2' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ko&#47;KomplainDashboard&#47;Dashboard2&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1531410432488');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1000px';vizElement.style.height='827px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+        </script>
     </div>
-    <!-- </body>
 
-        <div class="col-md-6">
+        <!-- <div class='tableauPlaceholder' id='viz1531410432488' style='position: relative'><noscript><a href='#'><img alt='Dashboard  ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ko&#47;KomplainDashboard&#47;Dashboard2&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='KomplainDashboard&#47;Dashboard2' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ko&#47;KomplainDashboard&#47;Dashboard2&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1531410432488');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1000px';vizElement.style.height='827px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+    </div> -->
+    
+
+        <div class="col-sm-6"><br>
+        <canvas id="myChart2" style="width: 508px; height: 400px;  border:1px solid #000000;"></canvas>
             
             <script>
-
-            var myLineChart = new Chart(ctx, {
+            var ctx = document.getElementById("myChart2");
+            var myChart2 = new Chart(ctx, {
                 type: 'line',
                 data: {
                     labels: ["Januari", "Febuari", "March", "April", "May","Juni","July"],
                     datasets: [
                         {
-                            label: "Percobaan Line Charts",
+                            label: "Data Komplain Bulanan",
                             fill: false,
                             lineTension: 0.1,
                             backgroundColor: "rgba(75,192,192,0,4)",
@@ -111,8 +114,35 @@
                 }
             });
             </script>
-        </div>
     </div>
-</div> -->
 
-        
+
+    <div class="col-sm-6"><br>
+        <canvas id="myChart3" style="width: 508px; height: 400px;  border:1px solid #000000;"></canvas>
+            <script>
+                var ctx = document.getElementById("myChart3");
+                var myChart3 = new Chart(ctx,{
+                    type: 'pie',
+                    data:{
+                        datasets: [{
+                            data: [10, 20, 30]
+                        }],
+                        labels: ['Red','Yellow','Blue'],
+                        datasets: [{
+                            label: "Population (millions)",
+                            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                            data: [2478,5267,734,784,433]
+                          }]
+                        },
+                        options: {
+                          title: {
+                            display: true,
+                            text: 'Predicted world population (millions) in 2050'
+                          }
+                        }
+                    });
+
+            </script>
+    </div>
+</div>
+</body>
